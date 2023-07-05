685119bacb9edc647163020aa69bd44aa3942c58
Sebastian Führ <s.fuehr1@gmail.com>
2023-07-05 10:50:57 +0300

    Stock market data pull and GUI improvements

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import customtkinter as ctk
 from settings import *
+import yfinance as yf
+from datetime import datetime
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 try:
     from ctypes import windll, byref, sizeof, c_int
@@ -14,10 +18,41 @@ class App(ctk.CTk):
 
         self.input_string = ctk.StringVar(value='AAPL')
         self.time_string = ctk.StringVar(value=TIME_OPTIONS[0])
+        self.time_string.trace('w', self.create_graph)
+        self.has_data = False
 
+        self.graph_panel = None
         InputPanel(self, self.input_string, self.time_string)
 
+        self.bind('<Return>', self.input_handler)
+
         self.mainloop()
+
+    def create_graph(self, *args):
+        if self.graph_panel: self.graph_panel.pack_forget()
+
+        if self.has_data:
+            match self.time_string.get():
+                case 'Max': data = self.max
+                case '1 Year': data = self.year
+                case '6 Months': data = self.six_months
+                case 'Month': data = self.one_month
+                case 'Week': data = self.one_week
+            self.graph_panel = GraphPanel(self, data)
+
+    def input_handler(self, event=None):
+        ticker = yf.Ticker(self.input_string.get())
+        start = datetime(1950, 1, 1) # For getting Max
+        end = datetime.today()
+
+        self.max = ticker.history(start=start, end=end, period='1d')
+        self.year = self.max.iloc[-260:]
+        self.six_months = self.max.iloc[-130:]
+        self.one_month = self.max.iloc[-22:]
+        self.one_week = self.max.iloc[-5:]
+
+        self.has_data = True
+        self.create_graph()
 
     def title_bar_color(self):
         try:
@@ -66,6 +101,33 @@ class TextButton(ctk.CTkLabel):
     def unselect(self):
         self.configure(text_color=TEXT_COLOR)
 
+class GraphPanel(ctk.CTkFrame):
+    def __init__(self, parent, data):
+        super().__init__(master=parent, fg_color=BG_COLOR)
+        self.pack(expand=True, fill='both')
+        
+        # Figure
+        figure = plt.Figure()
+        figure.subplots_adjust(left=0, right=0.99, top=1, bottom=0)
+        figure.patch.set_facecolor(BG_COLOR)
+
+        # Graph
+        ax = figure.add_subplot(111)
+        ax.set_facecolor(BG_COLOR)
+        for side in ['top', 'left', 'right', 'bottom']:
+            ax.spines[side].set_color(BG_COLOR)
+        
+        line = ax.plot(data['Close'])[0]
+        line.set_color(HIGHLIGHT_COLOR)
+
+        # Ticks
+        ax.tick_params(axis='x', direction='in', pad=-14, colors=TICK_COLOR)
+        ax.yaxis.tick_right()
+        ax.tick_params(axis='y', direction='in', pad=-22, colors=HIGHLIGHT_COLOR)
+
+        # Widget
+        figure_widget = FigureCanvasTkAgg(figure, master=self)
+        figure_widget.get_tk_widget().pack(fill='both', expand=True)
 
 if __name__ == '__main__':
     App()
